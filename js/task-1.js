ec81fae5cf16fd17515c91c4b6c624a124374ebc
Bohdan Haponiuk <jlomka54@gmail.com>
2024-07-18 12:03:19 +0400

    add style and js element

--- js/task-1.js
+++
@@ -1,7 +0,0 @@
-
-const items = document.querySelectorAll(".item");
-console.log(`Number of categorie : ${items.length}`);
-items.forEach(item => {
-    console.log('Category: ', item.querySelector('h2').textContent);
-    console.log('Elements: ', item.querySelectorAll('li').length);
-});
